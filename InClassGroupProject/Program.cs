e57ee5e66c94eb33e798d95ec75e60d6e99f1e77
Macy Draper <Macy>
2019-06-21 09:06:41 -0500

    Macys Name added

--- InClassGroupProject/Program.cs
+++ InClassGroupProject/Program.cs
@@ -11,6 +11,7 @@ namespace InClassGroupProject
 
             // Add all the students
             students.Add("Brandon W. Dowds");
+            students.Add("Macy J Draper");
 
 
             Random random = new Random();
