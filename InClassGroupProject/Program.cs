903b23227eda6779fec96869c5ac42981f7b05f2
Ben Gazzini <kenpanini@gmail.com>
2019-06-21 09:31:00 -0500

    added my name

--- InClassGroupProject/Program.cs
+++ InClassGroupProject/Program.cs
@@ -11,6 +11,7 @@ namespace InClassGroupProject
 
             // Add all the students
             students.Add("Brandon W. Dowds");
+            students.Add("Ben Gazzini");
 
 
             Random random = new Random();
