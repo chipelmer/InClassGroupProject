5783e7d44c851ccf61bb3f0e13842b20508e021b
Robert Hayes <robertaustinhayes@gmail.com>
2019-06-21 09:08:33 -0500

    Added Robert A. Hayes

--- InClassGroupProject/Program.cs
+++ InClassGroupProject/Program.cs
@@ -11,7 +11,7 @@ namespace InClassGroupProject
 
             // Add all the students
             students.Add("Brandon W. Dowds");
-
+            students.Add("Robert A. Hayes");
 
             Random random = new Random();
             int randomNumber = random.Next(0, students.Count);
