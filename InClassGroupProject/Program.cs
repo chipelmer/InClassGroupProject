6dd7f1d5f506029a26ca1d97dae6594487c9bf97
Chip <chip.elmer@gmail.com>
2019-06-21 08:49:44 -0500

    Initial version of random student picker

--- InClassGroupProject/Program.cs
+++ InClassGroupProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InClassGroupProject
 {
@@ -6,7 +7,16 @@ namespace InClassGroupProject
     {
         static void Main(string[] args)
         {
-            
+            List<string> students = new List<string>();
+
+            // Add all the students
+            students.Add("Brandon W. Dowds");
+
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, students.Count);
+
+            Console.WriteLine(students[randomNumber]);
         }
     }
 }
