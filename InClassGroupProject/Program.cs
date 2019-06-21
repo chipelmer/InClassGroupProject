05051a1ffd6aa0407ea64f4d62a19bd5cf71553a
Tanner <tanner.drayton01@gmail.com>
2019-06-21 09:08:23 -0500

    Added Tanner Riddle

--- InClassGroupProject/Program.cs
+++ InClassGroupProject/Program.cs
@@ -11,6 +11,7 @@ namespace InClassGroupProject
 
             // Add all the students
             students.Add("Brandon W. Dowds");
+            students.Add("Tanner D. Riddle");
 
 
             Random random = new Random();
